d41f688acfbbc96973e2028f7d794be2c8b0c04b
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-10-28 17:12:20 +0300

    add condition

---
+++ condition.js
@@ -0,0 +1,11 @@
+let temp = 30;
+
+if (temp > 27) {
+    alert("Today is hot day")
+} else if (temp < 27) {
+    alert("Today is a cold day")
+} else if (temp == 27) {
+    alert("Today is a calm day")
+} else {
+    alert("Error in the system")
+}
