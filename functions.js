5700d873ebecccf2cff26b549e09437538b6f11e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-10-28 17:12:48 +0300

    add funtions

---
+++ functions.js
@@ -0,0 +1,20 @@
+// function teamMembers(male, female) {
+//     total = male + female
+//     console.log(total)
+// }
+
+// teamMembers(89, 100)
+answer = prompt("Enter a greeting message: ")
+
+function greetPeople(message) {
+    console.log(message)
+}
+
+greetPeople(answer)
+
+const greetPeople = (message) => {
+    console.log(message)
+
+}
+
+const sum = (a, b) =>  a + b
