d192bd8c724c73252744fcad74d5bcbab6fca3b1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-10-28 17:11:04 +0300

    add array objects

---
+++ array-objects.js
@@ -0,0 +1,9 @@
+let mySchool = [
+    {name: "students", population: 34, class: 8},
+    {name: "teachers", population: 10, class: 8},
+    {name: "managers", population: 3, class: 8}
+]
+
+for (let item = 0; item < mySchool.length; item++) {
+    console.log("The name is: " + mySchool[item].population)
+}
