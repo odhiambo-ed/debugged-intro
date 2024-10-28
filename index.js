400888b73f2299d09d77626a839b1c1fcd31f194
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-10-28 17:14:12 +0300

    add index js

---
+++ index.js
@@ -0,0 +1,38 @@
+// let number = 4
+
+// if (number > 2) {
+//     console.log("We have a class quorum!")
+// } else if (number < 2) {
+//     console.log("We don't have a class quorum")
+// } else {
+//     console.log("Invalid number")
+// }
+
+// let item;
+
+// for (item = 0; item < 10; item++) {
+//     console.log("Number is " + item)
+// }
+
+// cars = ["BMW", "Benz", "VW", "Ford"]
+
+// for (item of cars) {
+//     console.log(item)
+// }
+
+// function greetingClass(name1,name2, name3) {
+//     console.log(name1 + " " + "Greetings from Finland")
+//     console.log(name2 + " " + "Greetings from Finland")
+//     console.log(name3 + " " + "Greetings from Finland")
+// }
+
+// greetingClass("Vansh", "Ali", "Ethan");
+
+
+const greetingClass = (name1, name2, name3) => {
+    console.log(name1 + " " + "Greetings from Finland")
+    console.log(name2 + " " + "Greetings from Finland")
+    console.log(name3 + " " + "Greetings from Finland")
+}
+
+greetingClass("Vansh", "Ali", "Ethan");
